ca796e3eefd941620b2e9093547ce6a11cbdff6c
WambuaJoe <kaytujohn254@gmail.com>
2023-12-02 12:05:47 +0300

    code fix - create a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_nd;
 
-	new_nd= malloc(sizeof(binary_tree_t));
+	new_nd = malloc(sizeof(binary_tree_t));
 
 	if (new_nd == NULL)
 		return (NULL);
